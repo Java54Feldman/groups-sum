0edc42b419067a5012d5e935c16d7fd00a790c2a
Pavel Feldman <feldmanpk@gmail.com>
2024-08-03 18:41:10 +0300

    getLargeGroups, performance tests for Executor Service & ThreadPool

--- src/telran/numbers/test/GroupsSumTest.java
+++ src/telran/numbers/test/GroupsSumTest.java
@@ -2,13 +2,16 @@ package telran.numbers.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.Test;
 
 import telran.numbers.*;
 
 class GroupsSumTest {
-	private static final int N_GROUPS = 1_000;
-	private static final int GROUP_LENGTH = 1_000;
+	private static final int N_GROUPS = 10_000;
+	private static final int GROUP_LENGTH = 10_000;
 	int[][] groups = {
 			{1,2},
 			{3,4},
@@ -21,16 +24,19 @@ class GroupsSumTest {
 		GroupsSum gs = new GroupsSumTaskThread(groups);
 		assertEquals(21, gs.getSum());
 	}
-	private int[][] getLargeGroups(int nGroups, int groupLength) {
-		// TODO 
-		// creating random two dimensional array
-		// using method generate of Stream
-		return null;
-	}
 	@Test
 	void groupSumThreadPoolTest() {
 		GroupsSum gs = new GroupsSumThreadPool(groups);
 		assertEquals(21, gs.getSum());
+	}
+	private int[][] getLargeGroups(int nGroups, int groupLength) {
+		// creating random two dimensional array
+		// using method generate of Stream
+		return IntStream.range(0, nGroups).mapToObj(i -> randomArray(groupLength)).toArray(int[][]::new);
+	}
+	private int[] randomArray(int numberOfElements) {
+		ThreadLocalRandom random = ThreadLocalRandom.current();
+		return IntStream.generate(random::nextInt).limit(numberOfElements).toArray();
 	}
 	@Test
 	void groupsSumTaskThreadPerformance() {
